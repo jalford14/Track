d0b47bce6b78ecad2b902eef327056730c8cca11
James Alford <jalford14@liberty.edu>
2017-07-02 18:36:57 -0500

    Asthetic Changes
    Continuing to make some minor changes. Also added a date function to show which day is added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
     one();
-    two();
-    
+    three();
 });
 function one() {
         $("#title")
-        .css('opacity', 0)
-        .slideDown('slow')
+        .hide()
+        .slideDown(750)
         .animate(
         { opacity: 1 },
         { queue: false, duration: 'slow' }
@@ -15,7 +14,23 @@ function one() {
 }
 
 function two () {
-    $("#line").animate({width: "500"}, 1000);
+    $("#line").animate({width: "500"}, 750);
     /*$('#line').animate({ opacity: 1, top: "-10px" }, 'slow');*/
-    
+    three();
 }
+
+function three() {
+    $("#form-style-5").delay(750).hide().fadeIn();
+}
+
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = d.getFullYear() + '/' +
+    ((''+month).length<2 ? '0' : '') + month + '/' +
+    ((''+day).length<2 ? '0' : '') + day;
+
+output = String(output);
+$("#demo").html(output);
